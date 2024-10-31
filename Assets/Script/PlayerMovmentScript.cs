a14f472759a0009472dcfbdda9beaefaeb2a16cf
26florma <159549887+26florma@users.noreply.github.com>
2024-10-30 20:18:51 -0400

    Update PlayerMovmentScript.cs
    Edits code to only run code on detection of drone on germ

--- Assets/Script/PlayerMovmentScript.cs
+++ Assets/Script/PlayerMovmentScript.cs
@@ -9,7 +9,6 @@ public class PlayerMomentScript : MonoBehaviour
     public GameObject germs;
     public int stemCellBar;
     public int DNA;
-    private bool isOnGerm;
 
     // Start is called before the first frame update
     void Start()
@@ -20,24 +19,20 @@ public class PlayerMomentScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 mousePos =Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = mousePos;
-        if (Input.GetKeyDown(KeyCode.Mouse0) && isOnGerm == true)
-        {
-            Debug.Log("Hit Detected");
-            DNA = 1;
-        }
-        if(DNA > stemCellBar)
-        {
-            isOnGerm= false;
-        }
+        
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
       if(collision.gameObject.CompareTag("germ"))
       {
-            isOnGerm = true;
+        Vector2 mousePos =Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = mousePos;
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Debug.Log("Hit Detected");
+            DNA = 1;
+        }
       }
       
     }
