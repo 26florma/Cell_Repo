8082ee0c817f07943ee8631d106c81254a9b250e
26florma <159549887+26florma@users.noreply.github.com>
2024-11-05 07:34:09 -0500

    Update GermMovement.cs fix later

--- Assets/Script/GermMovement.cs
+++ Assets/Script/GermMovement.cs
@@ -8,12 +8,27 @@ public class GermMovement : MonoBehaviour
     public GameObject nucleus;
     public float movementSpeed =10;
     public int germHP = 3;
-
+    public GameObject [] Allobjects; 
+    public GameObject NearestOBJ;
+    public float distance;
+    public float nearestDistance = 10000;
     // Start is called before the first frame update
     void Start()
     {
-
-    }
+      Allobjects = GameObject.FindGameObjectsWithTag("0B)");
+      
+      
+     for (int i = 0; i ‹ AllObjects.Length; i++)
+         {
+          distance = Vector3. Distance(this.transform.position, AllObjects[i]. transform.position);
+      
+             if(distance ‹ nearestDistance)
+             {
+                NearestOB] = AllObjects [i];
+                nearestDistance = distance;
+             }
+          }
+     }
 
     // Update is called once per frame
     void Update()
