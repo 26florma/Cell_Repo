2af18c0220a191d0383f6ad29bbabecc541f9485
26florma <159549887+26florma@users.noreply.github.com>
2024-11-05 20:22:22 -0500

    Error correction
    Fixed some errors with AllObjects, it was spelled with lowercase o in multiple places

--- Assets/Script/GermMovement.cs
+++ Assets/Script/GermMovement.cs
@@ -9,20 +9,20 @@ public class GermMovement : MonoBehaviour
     public GameObject nucleus;
     public float movementSpeed =10;
     public int germHP = 3;
-    public GameObject[] Allobjects; 
+    public GameObject[] AllObjects; 
     public GameObject NearestNucl;
     public float distance;
     public float nearestDistance = 10000;
     // Start is called before the first frame update
     void Start()
     {
-       Allobjects = GameObject.FindGameObjectsWithTag("Nucl");
+       AllObjects = GameObject.FindGameObjectsWithTag("Nucl");
       
-     for (int i = 0; i ‹ AllObjects.Length; i++)
+     for (int i = 0; i < AllObjects.Length; i++)
          {
           distance = Vector3. Distance(this.transform.position, AllObjects[i]. transform.position);
       
-             if(distance ‹ nearestDistance)
+             if(distance < nearestDistance)
             {
                 NearestNucl = AllObjects[i];
                 nearestDistance = distance;
