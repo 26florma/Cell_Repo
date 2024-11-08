b1eb22187581f4773e571f643f1a9e86b550b621
26florma <159549887+26florma@users.noreply.github.com>
2024-11-07 21:46:45 -0500

    Update NucleusScript.cs
    Added damage to nucleus

--- Assets/Script/NucleusScript.cs
+++ Assets/Script/NucleusScript.cs
@@ -5,20 +5,39 @@ using UnityEngine;
 public class NucleusScript : MonoBehaviour
 {
     public int NuclHP = 20;
+    
+    private bool hit = false;
     private bool NuclDead = false;
+    private GameObject germ;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        germ = GameObject.FindGameObjectWithTag("germ");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(NuclDead == true) 
+        if(NuclHP =< 0)
         {
-            Destroy(gameObject);
+            Destroy(gameOject);
         }
+    }
+    //detection for taking damage 
+    void TakeDamage()
+    {
+      if (hit = true)
+      {
+        NuclHP =- germ.Getcomponet<germD>;
+      }
+    }
+    //detection for hit
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+     if(collision.gameObject.CompareTag("germ"))
+     {
+       hit = true
+     }
     }
 }
