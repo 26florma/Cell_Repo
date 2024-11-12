abf68549a0f8f3ae9b3e3cc7b7a90b3930606861
26florma <159549887+26florma@users.noreply.github.com>
2024-11-11 22:00:05 -0500

    Created the first corner of the border tool
    Created the check for the first corner of the game border tool but there is still more work need for the tool, For Future Me: you need to create the checks for corner 2 and 3 and also set their position data to the positions of the closest corner or to corner 4

--- Assets/Script/GameBorderScript.cs
+++ Assets/Script/GameBorderScript.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class GameBorderScript : MonoBehaviour
 {
+
+     public GameObject corner1;
+     public GameObject Drone;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      if(Drone !=null && corner1 !=null)
+      {
+        if(Drone.transform.position.y >= corner1.transform.position.y)
+        {
+         vector3 Newposition = Drone.transfrom.position
+         Newposition.y = corner1.transform.position.y
+         Drone.transform.position.y = Newposition;
+        }
+      }
     }
 }
