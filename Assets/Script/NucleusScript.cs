626c980a2a746141764951a6ee660ada4ca0e2b1
26florma <159549887+26florma@users.noreply.github.com>
2024-11-16 15:02:14 -0500

    Error correction
    Fixed errors when having multiple germs in the scene

--- Assets/Script/NucleusScript.cs
+++ Assets/Script/NucleusScript.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class NucleusScript : MonoBehaviour
 {
     public int NuclHP = 20;
-    
+    public List<GameObject> attackingGerm = new List<GameObject>();
     private bool hit = false;
     
     private GameObject germ;
@@ -15,43 +15,57 @@ public class NucleusScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //delete if new solution works
-        germ = GameObject.FindGameObjectWithTag("germ");
+       
     }
 
     // Update is called once per frame
     void Update()
     {
+      if(attackingGerm != null && attackingGerm.Count > 0)
+      {
+           foreach(GameObject germ in attackingGerm)
+          {
+            if(germ != null)
+            {
+              GermMovement germMovement = germ.GetComponent<GermMovement>();
+              NuclHP -=germMovement.GermD;
+            }
+          } 
+       
+      }
+
         if(NuclHP <= 0)
         {
-            Destroy(gameObject);
-            germ.GetComponent<GermMovement>().NearestNucl = null;
-            germ.GetComponent<GermMovement>().FindNearestObject();
+             Destroy(gameObject);
+             if(attackingGerm.Count > 0)
+             {
+                GameObject AliveGerms = attackingGerm[0];
+                if(AliveGerms != null)
+                {
+                germ.GetComponent<GermMovement>().NearestNucl = null;
+                germ.GetComponent<GermMovement>().FindNearestObject();
+                }
+             }
         }
 
-    }
+     }
     //detection for hit
     void OnTriggerEnter2D(Collider2D collision)
     {
      if(collision.gameObject.CompareTag("germ"))
      {
-        hit = true;
-        for (int i = 0;  i <= NuclHP; i++ )
-        {
-                if (hit == true)
-                {
-                    NuclHP -= collision.gameObject.GetComponent<GermMovement>().germD;
-                    Debug.Log("nucleus detected hit");
-                }
-          
-        }
+           if(!attackingGerm.Contains(collision.gameObject))
+           {
+              attackingGerm.Add(collision.gameObject);
+           }
+
      }
     }
     void OnTriggerExit2D(Collider2D collision)
     {
      if(collision.gameObject.CompareTag("germ"))
      {
-            hit = false;
+              attackingGerm.Remove(collision.gameObject);
      }
     }
 }
