61d54967c380d0e250aedea13fc3a7a7c9f575d1
26florma <159549887+26florma@users.noreply.github.com>
2024-12-13 10:53:07 -0500

    Added Rhinovirus update to cellscript string

--- Assets/Script/Rhinovirus Script.cs
+++ Assets/Script/Rhinovirus Script.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class RhinovirusScript : MonoBehaviour
 {
+    public GameObject Cell;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,16 @@ public class RhinovirusScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Cell != null && cellIsAlive == false)
+        {
+        Cell.deadActions = "InfectedByVirus";
+        }
+    }
+    public void OnTriggerEnter2D(Collider CellObject)
+    {
+     If(CellObject.GameObject.CompareTag("Cell"))
+     {
+     CellObject = Cell
+     }
     }
 }
