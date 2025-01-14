4d111e01910da5add03f248b8cf696b6c090c8cb
26florma <159549887+26florma@users.noreply.github.com>
2025-01-14 14:04:53 -0500

    added Game Manager code
    game manager code is commented out add a new Game Manager script and use this

--- Assets/Script/Spawn Manager.cs
+++ Assets/Script/Spawn Manager.cs
@@ -12,7 +12,6 @@ public class SpawnManager : MonoBehaviour
     void Start()
     {
         AllSpawners = GameObject.FindGameObjectsWithTag("spawner").ToList();
-        
     }
 
     // Update is called once per frame
@@ -27,4 +26,57 @@ public class SpawnManager : MonoBehaviour
             }
         }
     }
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using Unity.VisualScripting.Antlr3.Runtime;
+
+public class SpawnManager : MonoBehaviour
+{
+   public bool mutations = false;
+   public int mutationCount = 3;
+   public list<AllPathogens>
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     AllPathogens = GameObject.FindGameObjectsWithTag("germ").ToList();
+     for(mutationCount > 0, mutationCount--)
+        {
+            int mutaion = UnityEngine.Random.Range(0,AllPathogens)
+            int MutationBuff = UnityEngine.Random.Range(1,4)
+            if (AllPathogens != null)
+            {
+                GermMovement MutationTarget = AllPathogens[mutation].GetComponent<GermMovement>();
+            }
+            Switch(MutationTarget)
+            {
+                case 1:
+                MutationTarget.germHP +=1
+                break;
+                case 2:
+                MutationTarget.germD +=1;
+                break 3;
+                case
+                MutationTarget.movementSpeed +=1;
+                break;
+                case 4:
+                MutationTarget.germAttackSpeed +=1;
+                break;
+                defualt:
+                Debug.Log("error");
+                break; 
+            }
+        }
+       
+        
+    }
+ }
+    */
 }
