10e50d756b00b68bd9f47f1dd22b7da3735c6463
26florma <159549887+26florma@users.noreply.github.com>
2025-01-16 14:33:41 -0500

    push

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using Unity.VisualScripting.Antlr3.Runtime;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public bool mutations = false;
+    public int mutationCount = 3;
+    public List<GameObject> AllPathogens = new List<GameObject>();
     void Update()
     {
-        
+        AllPathogens = GameObject.FindGameObjectsWithTag("germ").ToList();
+        for (int i = mutationCount; mutationCount > 0; mutationCount-= 1)
+        {
+            int mutation = UnityEngine.Random.Range(0, AllPathogens[]);
+            int MutationBuff = UnityEngine.Random.Range(1, 4);
+            if (AllPathogens != null)
+            {
+                GermMovement MutationTarget = AllPathogens[mutation].GetComponent<GermMovement>();
+
+                Switch(MutationTarget)
+               {
+                case 1:
+                MutationTarget.germHP += 1;
+                    break;
+                case 2:
+                MutationTarget.germD += 1;
+                    break;
+                case 3:
+                MutationTarget.movementSpeed += 1;
+                    break;
+                case 4:
+                MutationTarget.germAttackSpeed += 1;
+                    break;
+                    default:
+                Debug.Log("error");
+                    break;
+                }
+            }
+        }
+
+
     }
 }
